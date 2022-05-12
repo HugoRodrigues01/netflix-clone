cdcec35a462dfa94aeefa2a460dcabba81e63a90
Hugo Rodrigues Pereira <hugo999junha@gmail.com>
2022-05-12 00:13:10 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener("scroll", controlScrollFunctions)
+
+controlScrollFunctions()
+
+function controlScrollFunctions() {
+
+    onScrollFunction()
+
+}
+
+function onScrollFunction() {
+
+    if (scrollY > 0) {
+        mainmenu.classList.add("scroll")
+
+    } else {
+        mainmenu.classList.remove("scroll")
+    }
+
+}
