bfa11087cc874bdca73a60a98a64a5877b52a3df
Hugo Rodrigues Pereira <hugo999junha@gmail.com>
2022-05-16 23:13:38 -0300

    feat: Day 3 of building

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+// JQuery
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:6
+        }
+    }
+})
